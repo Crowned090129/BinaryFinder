b008bc7d6c2de337f30a6f7e894b5ae7204c7d19
luisr <luisrafaelcoronadox01@gmail.com>
2022-02-17 14:54:55 -0400

    fixes

--- BinaryFinder-Console/Program.cs
+++ BinaryFinder-Console/Program.cs
@@ -11,17 +11,17 @@ namespace BinaryFinder_Console
         public static void Main()
         {
             List<int> list = new List<int>();
-            
+
             for (int i = 0; i < 5555582; i++)
             {
-                list.Add( i == 0 ? i: i*2);
+                list.Add(i == 0 ? i : i * 2);
             }
-            
+
             int[] array = list.ToArray();
 
             Random rd = new Random();
 
-            int val =  rd.Next(0, array.Length);
+            int val = rd.Next(0, array.Length);
 
             //int[] order = BubbleSortAlgorithm_Library.BubbleSort.SortIntegers(array);
 
