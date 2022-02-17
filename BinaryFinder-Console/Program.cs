6f83ee3e61357658ec8744f6e05158ff722c5d06
luisr <luisrafaelcoronadox01@gmail.com>
2022-02-17 14:16:36 -0400

    fixes to the binary finder class

--- BinaryFinder-Console/Program.cs
+++ BinaryFinder-Console/Program.cs
@@ -10,7 +10,10 @@ namespace BinaryFinder_Console
     {
         public static void Main()
         {
-            BubbleSortAlgorithm_Library.BubbleSort.SortIntegers(new int[5]);
+            int[] array = { 5, 989, 14, 6454, 458, 1, 8, 9, 21, 47 };
+            int[] order = BubbleSortAlgorithm_Library.BubbleSort.SortIntegers(array);
+            (bool finded, int position) = BinarySearch.Library.BinarySearch.Find(order, 458);
+            Console.WriteLine(finded + " in position: " + position);
         }
     }
 }
---
+++ BinarySearch.Library/BinarySearch.cs
@@ -0,0 +1,30 @@
+﻿namespace BinarySearch.Library
+{
+    public class BinarySearch
+    {
+        public static (bool, int) Find(int[] obj, int num)
+        {
+            int lowIndex = 0;
+            int highIndex = obj.Length - 1;
+            int midIndex = Convert.ToInt32(((highIndex - lowIndex) / 2));
+            int finded = 0;
+
+            while (finded != num)
+            {
+                if (num == obj[midIndex])
+                    finded = obj[midIndex];
+                else if (num > obj[midIndex])
+                    lowIndex = midIndex + 1;
+                else if (num < obj[midIndex])
+                    highIndex = midIndex - 1;
+
+                midIndex = Convert.ToInt32(((highIndex - lowIndex) / 2)) + lowIndex;
+            }
+
+            if (finded == num)
+                return (true, midIndex);
+            else
+                return (false, 0);
+        }
+    }
+}
