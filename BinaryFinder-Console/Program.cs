d51d5182f5de9b46f2066838b00b62d5c9ccea9f
luisr <luisrafaelcoronadox01@gmail.com>
2022-02-17 12:43:13 -0400

    Add project files.

---
+++ BinaryFinder-Console/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryFinder_Console
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            BubbleSortAlgorithm_Library.BubbleSort.SortIntegers(new int[5]);
+        }
+    }
+}
